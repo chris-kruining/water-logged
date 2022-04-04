6618fcb9f571d45d2979e1e741e2c204f0547f8f
Chris Kruining <chris@kruining.eu>
2022-04-04 14:21:55 +0200

    feature: quick and dirty chalk-esc color implementation

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,32 @@ import { build, BuildOptions, BuildResult, Format, PluginBuild } from 'esbuild';
 export type Build = { format: Format, start: number, end: number, result: BuildResult };
 type BuildReportFunction = (value: (Build | PromiseLike<Build>)) => void;
 
-const toExtension = (format: Format) => ({ esm: 'mjs', cjs: 'cjs', iife: 'js' }[format]);
+const colors = {
+    reset: '\x1b[0m',
+    cyan: '\x1b[36m',
+    green: '\x1b[32m',
+    red: '\x1b[31m',
+};
+let _call!: (keyof typeof colors);
 
-const reset = '\x1b[0m';
-const cyan = '\x1b[36m';
-const green = '\x1b[32m';
-const red = '\x1b[31m';
+type Color = { (strings: TemplateStringsArray, ...args: any[]): string  }&{ [K in keyof typeof colors]: Color };
+
+const c: Color = new Proxy(() => {}, {
+    get: (target: any, property: string) => {
+        if(Object.keys(colors).includes(property) === false)
+        {
+            throw new Error(c.red`'${property}' is not a valid color!`);
+        }
+
+        _call = property as keyof typeof colors;
+
+        return c;
+    },
+    apply: (target: any, thisArg: any, [ strings, ...args ]: [ strings: TemplateStringsArray, ...args: any[] ]): string => {
+        return `${colors[_call]}${strings.flatMap((s, i) => [ s, args[i] ?? '' ]).join('')}${colors.reset}`;
+    },
+});
+const toExtension = (format: Format) => ({ esm: 'mjs', cjs: 'cjs', iife: 'js' }[format]);
 
 export async function compile(formats: Format[], options: Omit<BuildOptions, 'format'>)
 {
@@ -36,14 +56,14 @@ export async function compile(formats: Format[], options: Omit<BuildOptions, 'fo
         const results = builds
             .map(({ format, start, end, result }) => {
                 const isSuccessful = result.errors.length === 0;
-                const color = isSuccessful ? green : red;
+                const color = isSuccessful ? c.green : c.red;
                 const icon = isSuccessful ? '✓' : '⚠';
 
-                return `${color}${icon} '${format}'(${Math.round(end - start)}ms)${reset}`;
+                return color`${icon} '${format}'(${Math.round(end - start)}ms)`;
             })
             .join('\n');
 
-        console.log(`${cyan}🌊 Finished build (${Math.round(end - start)}ms total)\n${results}${reset}\n`);
+        console.log(c.cyan`🌊 Finished build (${Math.round(end - start)}ms total)\n${results}\n`);
     };
 
     return Promise.all(formats.map(format => {
